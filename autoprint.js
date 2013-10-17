3f49ea3f5a5d9b8c2bc51d9a2325e275dc9afb6c
flamengo <pattaragorn.kh@gmail.com>
2013-10-18 03:05:35 +0700

    Update autoprint.js

--- autoprint.js
+++ autoprint.js
@@ -1,18 +1,15 @@
 try {
 	var page = require('webpage').create(), jqueryPath = 'jquery.js', fs = require('fs'), files = fs.list('img'), imgs = [];
-
 	for (var i = 0, ii = files.length, j = 1; i < ii; i++)
 		if (/[A-Za-z]+\.[A-Za-z]+/.test(files[i])) {
 			imgs.push(files[i]);
 			console.log(j++ + ': ' + files[i]);
 		}
-		
 	page.paperSize = {
 		format : 'A4',
 		orientation : 'portrait',
 		margin : '1cm'
 	};
-
 	var content = '', template = '<html><body><style>table{page-break-before:always;width:100%;}tr{width:100%;}td{text-align:center;max-width:820px;min-height:20px;}img{width:100%;}.rotate{-webkit-transform:rotate(90deg);height:100%;width:auto;}</style><table>###imgs###</table></body></html>';
 	for (var i = 0, ii = imgs.length; i < ii; i++) {
 		if (!(i % 3) && i)
@@ -21,7 +18,6 @@ try {
 	}
 	fs.write('autoprint.html', template.replace('###imgs###', content), 'w');
 	fs.remove('autoprint.pdf');
-
 	page.open('autoprint.html', function(status) {
 		if (status !== 'success') {
 			console.log('Unable to load the address!');
